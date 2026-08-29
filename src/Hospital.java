e2e1747ccac749211659100d0ea22f7c9a680557
tizisch <tizischipani07@gmail.com>
2026-08-29 20:17:39 -0300

    1.0 punto 1

---
+++ src/Hospital.java
@@ -0,0 +1,48 @@
+public class Hospital{
+    public static void main(String[] args) {
+
+        Paciente p1 = new Paciente("123456789", "Tiziano", "osde");
+        Paciente p2 = new Paciente("987654321", "Gonza", "swiss medical");
+        Paciente p3 = new Paciente("999999999", "Alejo", "pami");
+
+        Medico m1 = new Medico("01", "dr junchi", "pediatra");
+        Medico m2 = new Medico("02", "dr chinti", "cardiologo");
+
+        m1.asignarPaciente(p1);
+        m1.asignarPaciente(p2);
+        m2.asignarPaciente(p1);
+        m2.asignarPaciente(p3);
+        System.out.println("pacientes del medico 1: ");
+        m1.mostrarPacientes();
+        System.out.println("cantidad: " + m1.cantidadPacientes());
+        System.out.println();
+
+        System.out.println("pacientes del medico 2: ");
+        m2.mostrarPacientes();
+        System.out.println("cantidad: " + m2.cantidadPacientes());
+        System.out.println();
+
+        System.out.println("busqueda");
+        Paciente buscado = m1.buscarPaciente("123456789");
+        if (buscado != null){
+            System.out.println("el paciente con el dni 123456789 es: " + buscado);
+        }else{
+            System.out.println("No existe tal paciente");
+        }
+        System.out.println();
+
+        System.out.println("eliminacion");
+        System.out.println("eliminar a tiziano con el dni 123456789");
+        boolean eliminado = m1.eliminarPaciente("123456789");
+        System.out.println("se elimino a tiziano?: " + eliminado);
+
+        System.out.println("pacientes de medico 1 despues de la eliminacion");
+        m1.mostrarPacientes();
+        System.out.println();
+
+        System.out.println("corroboracion de que el pacientes sigue existiendo: ");
+        System.out.println("el paciente 1 es: " + p1);
+        System.out.println("el paciente 1 sigue en la lista del medico 2: ");
+        m2.mostrarPacientes();
+    }
+}
---
+++ src/Medico.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Medico {
+    private String matricula;
+    private String nombre;
+    private String especialidad;
+    private List<Paciente> pacientes;
+
+    public Medico(String matricula, String nombre, String especialida) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.pacientes = new ArrayList<>();
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public List<Paciente> getPacientes() {
+        return pacientes;
+    }
+
+    public void setPacientes(List<Paciente> pacientes) {
+        this.pacientes = pacientes;
+    }
+
+    public Paciente buscarPaciente(String dni){
+        for (Paciente p : pacientes) {
+            if (p.getDni().equals(dni)) {
+                return p;
+            }
+        }
+        return null;
+    }
+    public boolean asignarPaciente(Paciente paciente){
+        if (paciente == null){
+            return false;
+        }
+        if (buscarPaciente(paciente.getDni()) != null){
+            return false;
+        }
+        this.pacientes.add(paciente);
+        return true;
+
+
+    }
+
+    public boolean eliminarPaciente(String dni){
+        if (buscarPaciente(dni) != null){
+            this.pacientes.remove(buscarPaciente(dni));
+            return true;
+        }
+        return false;
+    }
+
+    public int cantidadPacientes() {
+        return this.pacientes.size();
+    }
+
+
+
+    public void mostrarPacientes(){
+        if (!pacientes.isEmpty()) {
+            for (Paciente p : pacientes) {
+                System.out.println(p);
+            }
+        }else{
+            System.out.println("No tiene pacientes asignados");
+        }
+    }
+}
+
+
